b4520d5cd1ed91cd9491242b9da7ad8740e4986c
Francesca Barletta <wallebar@gmail.com>
2024-06-07 17:46:35 +0200

    ADD: technologies in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,6 +6,13 @@
         <p>Descrizione: {{ $project->descrizione }}</p>
         <p>Link: {{ $project->link }}</p>
         <p>Tipo: {{ optional($project->type)->name }}</p>
+        <p>Tecnologia:
+            @foreach($project->technologies as $tech)
+            <span>{{$tech->name}}</span>
+            @endforeach
+        </p>
+
+
 
     </div>
 @endsection
