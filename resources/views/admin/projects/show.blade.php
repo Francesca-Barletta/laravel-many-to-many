0a09d284ca62a9c375881c258e1d9aea9418570f
Francesca Barletta <wallebar@gmail.com>
2024-06-09 18:32:04 +0200

    ADD: style refactor

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,19 +2,27 @@
 @section('title','Francesca Barletta')
 @section('content')
     <div class="container-fluid show-content">
-        <div class="container">
+        <div class="container text-center">
             <h2 class="show-title">{{ $project->progetto }}</h2>
-   
-      
-            <p>Descrizione: {{ $project->descrizione }}</p>
-            <p>Link: {{ $project->link }}</p>
-            <p>Tipo: {{ optional($project->type)->name }}</p>
-            <p>Tecnologia:
+        </div>
+        <div class="container">
+        <div class="row row-cols-2">
+            <div class="col mh-100">
+                <p><h3>Descrizione:<br></h3> {{ $project->descrizione }}</p>
+                <p><h3>Repo Git-hub:<br></h3><a href="{{ $project->link }}">{{ $project->link }}</a></p>
+            </div>
+           <div class="col mh-100">
+            <p><h3>Tipo:<br></h3> {{ optional($project->type)->name }}</p>
+            <p><h3>Tecnologia:<br></h3>
                 @foreach($project->technologies as $tech)
                 <span>{{$tech->name}}</span>
                 @endforeach
             </p>
          
+           </div>
+        
+        </div>
+      
             <div class="proj-img">
                 @if($project->image)
                 <img src="{{ Vite::asset("resources/img/$project->image") }}" class="d-block w-100" alt="...">
