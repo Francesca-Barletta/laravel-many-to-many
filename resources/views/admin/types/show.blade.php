b7c55bd6ff8d8f8578cea6175e2e5388429e31a1
Francesca Barletta <wallebar@gmail.com>
2024-06-09 18:50:31 +0200

    ADD: style in show type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <h2>Tipologia:</h2>
-        <p>{{ $type->name}}</p>
-    </div>
+    <div class="container-fluid show-content">
+        <div class="container">
+            <h2 class="show-title">Tipologia:</h2>
+            <p>{{ $type->name}}</p>
+        </div>
+        </div>
+       
 @endsection
--- resources/views/guest/projects/show.blade.php
+++ resources/views/guest/projects/show.blade.php
@@ -2,18 +2,27 @@
 @section('title','Francesca Barletta')
 @section('content')
 <div class="container-fluid show-content">
-    <div class="container">
-
+    <div class="container text-center">
         <h2 class="show-title">{{ $project->progetto }}</h2>
-   
-        <p><span class="h4">Descrizione:</span><br> {{ $project->descrizione }}</p>
-        <p><span class="h4">Link: </span><br> {{ $project->link }}</p>
-        <p><span class="h4">Tipo: </span><br>{{ optional($project->type)->name }}</p>
-        <p><span class="h4">Tecnologia: </span><br>
+    </div>
+    <div class="container">
+    <div class="row row-cols-2">
+        <div class="col mh-100">
+            <p><h3>Descrizione:<br></h3> {{ $project->descrizione }}</p>
+            <p><h3>Repo Git-hub:<br></h3><a href="{{ $project->link }}">{{ $project->link }}</a></p>
+        </div>
+       <div class="col mh-100">
+        <p><h3>Tipo:<br></h3> {{ optional($project->type)->name }}</p>
+        <p><h3>Tecnologia:<br></h3>
             @foreach($project->technologies as $tech)
             <span>{{$tech->name}}</span>
             @endforeach
         </p>
+     
+       </div>
+    
+    </div>
+  
         <div class="proj-img">
             @if($project->image)
             <img src="{{ Vite::asset("resources/img/$project->image") }}" class="d-block w-100" alt="...">
@@ -21,12 +30,10 @@
             <p>Non ci sono immagini del progetto</p>
             @endif
         </div>
-        
-                                                 
-
         <a href="{{ route('projects.index') }}"class="text-decoration-none btn btn-back">Indietro</a>
+
+
     </div>
    
-
 </div>
 @endsection
