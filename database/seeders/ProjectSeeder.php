4bb79349366e0490ea3360b3c2f4dba7038dc742
Francesca Barletta <wallebar@gmail.com>
2024-06-10 17:05:47 +0200

    ADD: added file project csv and read of csvfile in projectSeeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -15,6 +15,42 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-       
+        $data = $this->getCSVData(__DIR__ . '/projects.csv');
+
+        foreach($data as $index => $row) {
+            
+            if($index !== 0){
+                $project = new Project;
+    
+                $project->progetto = $row[0];
+                $project->slug = $row[1];
+                $project->descrizione = $row[2];
+                $project->link = $row[3];
+                $project->image = $row[4];
+            
+                $project->save();
+                
+            }
+        }
+    }
+
+    public function getCSVData(string $path) {
+
+        $data = [];
+
+        $file_stream = fopen($path, 'r');
+        if($file_stream === false){
+            exit('cannot open the file'. $path);
+
+        }
+
+        while(($row = fgetcsv($file_stream)) !== false){
+            $data[] = $row;
+        }
+
+        fclose($file_stream);
+        // dump($data);
+        return $data;
+
     }
 }
